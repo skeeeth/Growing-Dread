c1c70922690d961ffb71bf017989c8d3cf4b55f4
EHilly <emmet.hilly@gmail.com>
2024-10-04 11:01:44 -0500

    Improve sheep AI and sprite sorting
    The sheep RigidBody2D is now bouncy, so they will bounce off walls instead of continuously walking into them. However, it looks weird if the bounce happens immediately, so we instead save the new bounce direction and transition to the idle state.

--- Ranching/sheep.gd
+++ Ranching/sheep.gd
@@ -20,22 +20,19 @@ var state:SheepState;
 @export var wandering_acceleration:float
 @export var wandering_speed:float
 
-var target_velocity:Vector2
+var target_wandering_velocity:Vector2
 var facing_direction:String = "right"
+var recently_collided:bool = false
+
 
 func _ready():
 	state = SheepState.Idle
-	$WanderIdleEatTimer.start(max_idle_time)
+	$WanderIdleEatTimer.start(min_idle_time)
+
 
 func _on_wander_idle_eat_timer_timeout():
 	if (state != SheepState.Idle): #If eating or wandering, always transition to idling
-		state = SheepState.Idle
-		$WanderIdleEatTimer.wait_time = randf_range(min_idle_time, max_idle_time)
-		
-		linear_velocity = Vector2.ZERO
-		
-		$SheepSprite/AnimationPlayer.play("walk_" + facing_direction)
-		$SheepSprite/AnimationPlayer.stop()
+		start_idling()
 	elif (randf() > 0.7): #If idling, 30% chance of eating
 		state = SheepState.Eating
 		$WanderIdleEatTimer.start(randi_range(min_eating_cycles, max_eating_cycles) * $SheepSprite/AnimationPlayer.get_animation("eating_" + facing_direction).length)
@@ -47,10 +44,20 @@ func _on_wander_idle_eat_timer_timeout():
 		state = SheepState.Wandering
 		$WanderIdleEatTimer.wait_time = randf_range(min_wandering_time, max_wandering_time)
 		
-		target_velocity = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized() * wandering_speed
-		change_facing(target_velocity)
+		target_wandering_velocity = pick_wandering_direction() * wandering_speed
+		change_facing(target_wandering_velocity)
 		
 		$SheepSprite/AnimationPlayer.play("walk_" + facing_direction)
+
+
+func start_idling():
+	state = SheepState.Idle
+	$WanderIdleEatTimer.wait_time = randf_range(min_idle_time, max_idle_time)
+	
+	linear_velocity = Vector2.ZERO
+	
+	$SheepSprite/AnimationPlayer.play("walk_" + facing_direction)
+	$SheepSprite/AnimationPlayer.stop()
 
 
 func change_facing(new_velocity:Vector2):
@@ -65,7 +72,26 @@ func change_facing(new_velocity:Vector2):
 		else:
 			facing_direction = "up"
 
+
+func pick_wandering_direction():
+	if (recently_collided): #When the collision happened, the sheep bounced away from the colliding object and we saved that new velocity as target_wandering_velocity
+							#Let's keep going in that direction to avoid bumping into that obstacle again 
+		recently_collided = false
+		return target_wandering_velocity.normalized()
+	else:
+		return Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
+
+
 func _physics_process(delta):
 	if (state == SheepState.Wandering):
-		if (linear_velocity.length() < target_velocity.length()):		
-			apply_force(target_velocity.normalized() * wandering_acceleration * delta)
+		if ((linear_velocity-target_wandering_velocity).length() > 0.1):
+			apply_force(target_wandering_velocity.normalized() * wandering_acceleration * delta)
+
+
+func _on_body_entered(body):
+	if (state == SheepState.Wandering):
+		#This logic expects the Rigidbody to be bouncy so that the sheep starts walking away from the obstacle
+		#However, it looks unnatural if the sheep walks into a wall and immediately starts going in the other direction, so let's save the new direction and switch to idling
+		recently_collided = true
+		target_wandering_velocity = linear_velocity.normalized() * wandering_speed
+		start_idling()
