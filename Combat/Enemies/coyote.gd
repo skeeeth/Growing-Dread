c3fa0358f4e431caac85f2afd2ef6a56ba3bd83c
ar13l-x07 <ar13l.x07@gmail.com>
2024-10-16 08:25:34 -0500

    add coyote

--- Combat/Enemies/coyote.gd
+++ Combat/Enemies/coyote.gd
@@ -2,32 +2,17 @@ class_name Wolf
 extends CharacterBody2D
 
 signal died(id)
-# todo: add sneaking state :todo
-enum WolfState { Chilling, Chasing, Biting, Dragging, Dead, }
 
-var state:int = WolfState.Chilling:
-	set(a_state):
-		match a_state:
-			WolfState.Chilling:
-				state = a_state
-				$coyote_image.animation = "idle"
-			WolfState.Chasing:
-				state = a_state
-				$coyote_image.animation = "run"
-			WolfState.Biting:
-				state = a_state
-				$FarmableTile/CropDisplay.animation = "attack"
-			WolfState.Dragging:
-				state = a_state
-				$FarmableTile/CropDisplay.animation = "uhhhhhh...."
-			WolfState.Dead:
-				state = a_state
-				$FarmableTile/CropDisplay.animation = "uhhhhhh...."
-			_:
-				print("Warning : farmable_tile.gd::_set_state(...)! Undefined farmable_tile state!")
-	get:
-		return state
-		
+enum WolfState {
+	Chilling,
+	#Sneaking, #Would be cool to add a sneaking state where it walks towards the target_sheep. Switch from sneaking to chasing once the sheep starts to flee
+	Chasing,
+	Biting,
+	Dragging,
+	Dead,
+}
+
+var state:WolfState
 
 @export var bite_distance:float
 @export var chasing_speed:float
@@ -59,28 +44,27 @@ func _ready():
 		_start_chasing()
 	else:
 		state = WolfState.Chilling
-	$coyote_image.play()
 
 
 func _start_chasing():
 	state = WolfState.Chasing
-	#$Sprite2D/AnimationPlayer.play("run_right")
+	$Sprite2D/AnimationPlayer.play("run_right")
 
 
 func _start_biting():
 	state = WolfState.Biting
 	
-	#$Sprite2D/AnimationPlayer.play("bite_right")
+	$Sprite2D/AnimationPlayer.play("bite_right")
 	
 	#Wait until we're 70% through the bite animation, which is roughly where the bite itself happens
-	#$BitingTimer.start($Sprite2D/AnimationPlayer.get_animation("bite_right").length*0.7)
+	$BitingTimer.start($Sprite2D/AnimationPlayer.get_animation("bite_right").length*0.7)
 	await $BitingTimer.timeout
 	
 	if (state == WolfState.Biting):
 		target_sheep.become_immobilized(self)
 		state = WolfState.Dragging
 		velocity = Vector2.LEFT * dragging_speed
-		#$Sprite2D/AnimationPlayer.play("drag_right")
+		$Sprite2D/AnimationPlayer.play("drag_right")
 		dragging_initial_sheep_displacement = target_sheep.global_position - global_position
 
 
@@ -97,4 +81,3 @@ func _physics_process(delta):
 	
 	if (state == WolfState.Dragging):
 		target_sheep.global_position = global_position + dragging_initial_sheep_displacement #Bring the sheep along by keeping the same relative position
-
