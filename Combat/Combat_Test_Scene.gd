e164f640f2edb0949b5853291d8bea78938719b6
skeeth <ethanhalbe@gmail.com>
2024-10-17 17:41:15 -0500

    Farming Events and trees

--- Combat/Combat_Test_Scene.gd
+++ Combat/Combat_Test_Scene.gd
@@ -5,7 +5,6 @@ const CORN = preload("res://Farming/FarmingTiles/Crops/Corn.tres")
 ##@onready var tile_map = $TileMap
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
 	#for n in get_children(true):
 		#if n is FarmTile:	
 			#n.till()
--- Farming/Tree.gd
+++ Farming/Tree.gd
@@ -1,12 +1,22 @@
 extends Area2D
+class_name EyeTree
 
-
+@export var forward_margin:float = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$farmer_image.animation = "Look_Straight"
-	$farmer_image.play()
+	$tree_image.animation = "Look_Straight"
+	$tree_image.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	#look_at_position(get_global_mouse_position())
 	pass
+
+func look_at_position(pos:Vector2): #in global position
+	if global_position.x - pos.x > 100:
+		$tree_image.play("Look_Left")
+	elif global_position.x - pos.x < -100:
+		$tree_image.play("Look_Right")
+	else:
+		$tree_image.play("Look_Straight")
