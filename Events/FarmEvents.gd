6c134763c6211b573b2677a47a19a2bfaa341186
skeeth <ethanhalbe@gmail.com>
2024-10-18 14:50:21 -0500

    Mite Squelch

--- Events/FarmEvents.gd
+++ Events/FarmEvents.gd
@@ -8,6 +8,7 @@ var farm_tiles:Array[FarmTile]
 @export_group("HarvestSquelch","squelch_")
 @export var squelch_req:int
 @export var squelch_repeat:Vector2
+@export var squelch_mite_sprite:Texture2D
 const SQUISH_SOUND = preload("res://Assets/Sounds/Squish.wav")
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +29,16 @@ func on_harvest(source):
 		squelch_req = randi_range(squelch_repeat.x,squelch_repeat.y)
 		var previous_sound = tile.harvest_sound.stream
 		tile.harvest_sound.stream = SQUISH_SOUND
+		for i in range(0,12):
+			var new_sprite = Sprite2D.new()
+			new_sprite.texture = squelch_mite_sprite
+			new_sprite.scale = Vector2(0.1,0.1)
+			var leave = create_tween()
+			var end_position = tile.global_position +\
+					 Vector2(randf_range(-1000.0,1000.0),randf_range(-800,2000))
+			leave.tween_property(new_sprite,"global_position",end_position,3.0)
+			leave.tween_callback(new_sprite.queue_free)
+			tile.add_child(new_sprite)
 		await tile.harvest_sound.finished
 		tile.harvest_sound.stream = previous_sound
 		
